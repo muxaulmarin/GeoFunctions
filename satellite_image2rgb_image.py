e9b97ace0f9f87a48ee57a071b6eddb13be29f54
Mikhail Marin <muxaul.marin@gmail.com>
2020-12-25 14:39:33 +0500

    Update satellite_image2rgb_image.py

--- satellite_image2rgb_image.py
+++ satellite_image2rgb_image.py
@@ -1,10 +1,12 @@
+import numpy as np
+
 def satellite_image2rgb_image(satellite_image, bands=[3,2,1], quantiles=[0.001, 0.999]):
     '''
     satellite_image - 3D numpy array ROWS x COLS x CHANNELS
     bands - list of bands. bands[0] -> red channel, bands[1] -> green channel, bands[2] -> blue channel
     quantiles - quantiles for clipping band
     '''
-    import numpy as np
+    h, w, c = satellite_image.shape
     rgb_image = []
     for band in bands:
         channel = satellite_image[:, :, band]
@@ -12,6 +14,6 @@ def satellite_image2rgb_image(satellite_image, bands=[3,2,1], quantiles=[0.001, 
         channel = np.clip(channel, *channel_quantiles)
         channel = ((channel - channel.min()) / (channel.max() - channel.min()) * 255)
         channel = channel.astype(np.uint8)
-        channel = channel.reshape(512, 512, 1)
+        channel = channel.reshape(h, w, 1)
         rgb_image.append(channel)
     return np.concatenate(rgb_image, axis=2)
